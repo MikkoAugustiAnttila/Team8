c75b1d62103d2d47db18d066f74319b6844fbb8a
Mikko Anttila <fmian007@edu.xamk.fi>
2023-10-07 11:19:29 +0300

    Working Dialog Box that can receive and display text (with fancy text appearing)

--- Team8/Assets/basicManagement.cs
+++ Team8/Assets/basicManagement.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class basicManagement : MonoBehaviour
@@ -11,12 +13,59 @@ public class basicManagement : MonoBehaviour
     public static basicManagement basemanagement;
     public GameObject projectile;
     private GameObject pivot;
+
+    public bool enableTextBox;
+    [SerializeField] private TextMeshProUGUI textBox;
+    public float letterDelay = 0.1f;
+    private int index;
+    
     
 
     private void Awake()
     {
         basemanagement = this;
         pivot = GameObject.FindGameObjectWithTag("pivot");
+    }
+
+    private void Update()
+    {
+        if (enableTextBox == true)
+        {
+            textBox.enabled = true;
+        }
+        else
+        {
+            textBox.enabled = false;
+        }
+    }
+    
+    public void DialogChunk(params string[] chunkSet)
+    {
+        enableTextBox = true;
+        StartCoroutine(DisplayChunksWithDelay(chunkSet));
+    }
+
+    private IEnumerator DisplayChunksWithDelay(string[] chunkSet)
+    {
+        foreach (string chunk in chunkSet)
+        {
+            yield return StartCoroutine(DisplayTextLetterByLetter(chunk));
+            // Pause for a moment before displaying the next chunk
+            yield return new WaitForSeconds(5.0f);
+        }
+
+        enableTextBox = false;
+    }
+
+    private IEnumerator DisplayTextLetterByLetter(string text)
+    {
+        textBox.text = ""; // Clear the text box
+
+        foreach (char letter in text)
+        {
+            textBox.text += letter;
+            yield return new WaitForSeconds(letterDelay);
+        }
     }
 
     public void createProjectile()
